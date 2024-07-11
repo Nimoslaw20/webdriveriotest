2ba9df2a36d056985e0e2206a6b70d94c3dc5bc1
EmmanuelNimo <emmanuel.nimo@getampla.com>
2024-07-11 13:07:34 +0000

    add npm test

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -9,6 +9,7 @@ import mediPage from "../pageobjects/medi.page.ts";
 describe("Mediboard login", () => {
     it("tests Mediboard login", async () => {
       await mediPage.open()
+    
       console.log("passed open")
       await mediPage.verifyUrl()
       console.log("passed verify")
@@ -19,5 +20,9 @@ describe("Mediboard login", () => {
       await mediPage.getGeneralOverviewText();
     });
   });
-  
+
+
+
+  //command to run: npx wdio run ./wdio.conf.js
+  // or if configures in package.json: npm test
 
